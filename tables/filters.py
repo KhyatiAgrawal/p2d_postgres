1d6fbcaafc0921793f853b8fd45f4739d1b94f72
Khyati Agrawal <khyatia@princeton.edu>
2019-11-12 18:13:16 -0500

    Filter code'

--- tables/filters.py
+++ tables/filters.py
@@ -17,3 +17,17 @@ class UserFilter(django_filters.FilterSet):
  class Meta:
   model = UserInfo
   fields = ['gender', 'email', 'phone']
+
+
+ def CustomFilter(myDict):
+ 	dress_filter = Dress.objects.all().order_by('id')
+ 	for key in myDict:
+ 		if key == 'occasion':
+ 			dress_filter = dress_filter.filter(reduce(operator.or_, (Q(occasions__contains=x) for x in myDict[key])))
+ 		elif key == 'size':
+ 			dress_filter = dress_filter.filter(reduce(operator.or_, (Q(size__contains=x) for x in myDict[key])))
+ 		elif key == 'keyword':
+ 			dress_filter = dress_filter.filter(reduce(operator.or_, (Q(description__contains=x) for x in myDict[key])))
+ 		else:
+ 			dress_filter = dress_filter.filter(price__lte = myDict[key])
+ 	return dress_filter
