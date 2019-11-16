23aee4fd63812eebcf448346771d05a54ca7c472
Urvashi Uberoy <uuberoy@nat-oitwireless-inside-vapornet100-10-9-64-219.princeton.edu>
2019-11-16 14:41:45 -0500

    latest frontend changes

--- p2d-react/src/components/DressDisplay/DressDisplay.js
+++ p2d-react/src/components/DressDisplay/DressDisplay.js
@@ -10,53 +10,84 @@ const API_URL = 'https://localhost:8000';
 class DressDisplay extends Component {
   constructor(props) {
     super(props)
-    let dresses = {}
-    for (var key in props.dresses) {
-      dresses[key] = props.dresses[key]
-      dresses[key].modal = false
-      dresses[key].size = props.dresses[key].size
-      dresses[key].brand = props.dresses[key].brand
-      dresses[key].availability = props.dresses[key].availability
-      dresses[key].occasion = props.dresses[key].occasion
-    }
     this.state = {
-      dresses: dresses,
+      dresses: {},
     }
-  }
-
-  componentWillReceiveProps({dresses}) {
-    let dress_data = {}
-    for (let i in dresses) {
-      dress_data[i] = {
-        ...dresses[i],
-        modal: false
-      }
-    }
-    this.setState({
-      dresses: dress_data,
-    })
   }
 
   removeFromCart = (id) => {
     axios({method: 'delete', url: `${API_URL}/api/cart/`, data: {'dressToDelete': id}})
-    // axios.delete(`${API_URL}/api/cart/`, {'dressToDelete': id})
+    if (this.props.cart)
+      this.fetchDresses("cart");
+    else
+      this.fetchDresses("favorites");
   }
 
   removeFromFavorites = (id) => {
     axios({method: 'delete', url: `${API_URL}/api/favorites/`, data: {'dressToDelete': id}})
-    // axios.delete(`${API_URL}/api/cart/`, {'dressToDelete': id})
+    if (this.props.cart)
+      this.fetchDresses("cart");
+    else
+      this.fetchDresses("favorites");
+  }
+
+  fetchDresses = async (stem) => {
+    let res = await axios.get(`${API_URL}/api/${stem}/`)
+    console.log(res.data)
+    let dress_data = {}
+    let amount = 0
+    let total = 0
+    for (let i in res.data) {
+      dress_data[i] = {
+        id: res.data[i]["id"],
+        0: API_URL + "/" + res.data[i]["view1"],
+        1: API_URL + "/" + res.data[i]["view2"],
+        2: API_URL + "/" + res.data[i]["view3"],
+        title: res.data[i]["title"],
+        selected: 0,
+        total: 3,
+        brand: res.data[i]["brand"],
+        size: res.data[i]["size"],
+        description: res.data[i]["description"],
+        occasion: res.data[i]["occasions"].split(/(\s+)/),
+        price: res.data[i]["price"],
+        availability: res.data[i]["unavailableDates"]
+      }
+      amount += parseInt(dress_data[i]["price"])
+      total += 1
+    }
+    if (this.mounted) {
+      this.setState({dresses: dress_data, amount: amount, total: total})
+    }
+    if (this.props.cart) {
+      this.props.handleTotal(this.state.total, this.state.amount, this.state.dresses)
+    } else {
+      this.props.handleTotal(this.state.total)
+    }
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    if (this.props.cart)
+      this.fetchDresses("cart");
+    else
+      this.fetchDresses("favorites");
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
   }
 
 	render() {
     return (
       <div className="dress-display">
         {
-          Object.keys(this.props.dresses).map((key, index) => (
+          Object.keys(this.state.dresses).map((key, index) => (
             <div className="dress-display__img-container">
               <img className="dress-display__img" src={this.state.dresses[index][0]} />
               <div className="dress-display__text">
                 <div className="dress-display__title">{this.state.dresses[index].title}</div>
-                <div><p>Our rental price: $15.00</p></div>
+                <div><p>Our rental price: ${this.state.dresses[index].price}.00</p></div>
                 <div>{"Size: " + this.state.dresses[index].size}</div>
                 <div>{"Occasion: " + this.state.dresses[index].occasion}</div>
                 <div>{"Brand: " + this.state.dresses[index].brand}</div>
@@ -65,7 +96,7 @@ class DressDisplay extends Component {
                   <div className="cart-button__content">Remove from cart</div>
                 </div>
                 <div className="cart-button" style={this.props.favorites ? { display: 'flex'} : {display: 'none'}} onClick={() => {this.removeFromFavorites(this.state.dresses[index].id)}}>
-                  <div className="cart-button__content">Remove from favorites</div>
+                  <div className="cart-button__content">Remove</div>
                 </div>
               </div>
             </div>
